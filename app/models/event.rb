3a11cdab7d6acf1c705d8b810d4f3382c30b2327
Ryan Endacott <rzeg24@gmail.com>
2013-03-19 18:07:49 -0500

    Added presence validations for events, and date cannot be in past validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,14 @@
 class Event < ActiveRecord::Base
   attr_accessible :date, :description, :location, :name
+
+  validates_presence_of :date, :description, :location, :name
+
+  validate :event_date_cannot_be_in_the_past
+
+  def event_date_cannot_be_in_the_past
+    if !date.blank? and date < Date.today
+      errors.add(:date, "can't be in the past")
+    end
+  end
+
 end
