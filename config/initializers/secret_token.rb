51ba12d3602c85e0432b8790bf5ff11a6555bd8a
Ryan Endacott <rzeg24@gmail.com>
2013-03-19 16:42:30 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mizzouacm::Application.config.secret_token = '77ee93f7d8226243dd62a57abaedb506e3e72e6484f801bffb0241e9ff136f7efad86d367d7aa6fc673be6fe215844d2ec5a264af11e0c9b70fa22d09a02e3fd'
