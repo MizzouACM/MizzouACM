193831e112690f35a200be1da56fc2a0d09dad55
Ryan Endacott <rzeg24@gmail.com>
2013-03-19 17:56:57 -0500

    Test for User#is_admin? method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#is_admin?' do
+
+    let(:user) { FactoryGirl.create(:user) }
+    let(:admin) { FactoryGirl.create(:user, :admin => true) }
+
+    it 'should return true if is_admin' do
+      admin.is_admin?.should be_true
+    end
+
+    it 'should return false if not_admin' do
+      user.is_admin?.should be_false
+    end
+  end
 end
