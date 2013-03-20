b7d3938faaebe562a465a6c29ccd1688339ecc17
Ryan Endacott <rzeg24@gmail.com>
2013-03-19 21:23:56 -0500

    Removed github scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.production?
-    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], :scope => "user"
+    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
   else
     provider :developer unless Rails.env.production?
   end
