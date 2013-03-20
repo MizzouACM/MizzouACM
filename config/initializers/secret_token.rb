bb125edeea3b8ee79263f75ca2b46123347ad340
Ryan Endacott <rzeg24@gmail.com>
2013-03-19 19:29:52 -0500

    Made secret token that is not in repo be used in production

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,13 @@
 # Be sure to restart your server when you modify this file.
 
+if Rails.env.production? && ENV['SECRET_TOKEN'].blank?
+  raise 'SECRET_TOKEN environment variable must be set!'
+end
+
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Mizzouacm::Application.config.secret_token = '77ee93f7d8226243dd62a57abaedb506e3e72e6484f801bffb0241e9ff136f7efad86d367d7aa6fc673be6fe215844d2ec5a264af11e0c9b70fa22d09a02e3fd'
+Mizzouacm::Application.config.secret_token = 
+  ENV['SECRET_TOKEN'] || 'dceba89e94d58923984d01ea727584f25aee64f85c39ff3a069e25b16b086a6057c061a15915eafb4dcf4abeb4e5bd336453aef88907d7bddd4259b2e489dd94'
+
