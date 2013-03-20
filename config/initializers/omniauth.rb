ded870e52a8fa7ea9c2fd4d17dfa5a03965cbf59
Ryan Endacott <rzeg24@gmail.com>
2013-03-19 21:06:20 -0500

    Omniauth request name and email from github hopefully

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.production?
-    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], :scope => "user"
   else
     provider :developer unless Rails.env.production?
   end
