621f278e3aca1cf22945e7058b458828a112ef2b
Ryan Endacott <rzeg24@gmail.com>
2013-03-19 20:50:30 -0500

    Request username and email from github

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,44 @@
+require 'net/http'
+require 'json'
+
+
 class User < ActiveRecord::Base
   attr_accessible :provider, :uid, :name, :email
   attr_accessible :admin unless Rails.env.production?
 
   def self.create_with_omniauth(auth)
+
+    # Get github info
+    if Rails.env.production?
+      info = get_and_save_github_info(auth['uid']) 
+    else
+      info = auth['info']
+    end
+
     create! do |user|
       user.provider = auth['provider']
       user.uid = auth['uid']
-      if auth['info']
-         user.name = auth['info']['name'] || ""
-         user.email = auth['info']['email'] || ""
-      end
+      user.name = info[:name] 
+      user.email = info[:email]
     end
+
   end
 
   def is_admin?
     admin
+  end
+
+  private
+
+  def get_and_save_github_info(token)
+
+    url = URI.parse("api.github.com/users?access_token=#{token}")
+    req = Net::HTTP::Get.new(url.path)
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
+    return JSON.parse(res.body)
+
   end
 
 end
