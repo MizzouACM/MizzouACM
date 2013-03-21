5a84a108a4d1ec8e767556ed8ac343c9780c3967
Ryan Endacott <rzeg24@gmail.com>
2013-03-20 22:33:16 -0500

    Tests for event validations, TODO:  test date validation

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe Event do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:location) }
+  it { should validate_presence_of(:date) }
 end
