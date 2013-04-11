ad69ddf786c00e3f10969f943404945f27b0413f
Ryan Endacott <rzeg24@gmail.com>
2013-04-11 00:47:44 -0500

    Events no longer validate in the past... It sucked when you wanted to edit the description of an old event..

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,12 +3,4 @@ class Event < ActiveRecord::Base
 
   validates_presence_of :date, :description, :location, :name
 
-  validate :event_date_cannot_be_in_the_past
-
-  def event_date_cannot_be_in_the_past
-    if date and date < DateTime.now
-      errors.add(:date, "can't be in the past")
-    end
-  end
-
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -8,16 +8,4 @@ describe Event do
 
   let(:event) { FactoryGirl.build(:event) }
 
-  describe 'event_date_cannot_be_in_past validation' do
-    it 'should return false for a past date' do
-      event.date = DateTime.yesterday
-      event.should_not be_valid
-    end
-
-    it 'should be valid with a future date' do
-      event.date = DateTime.tomorrow
-      event.should be_valid
-    end
-  end
-
 end
