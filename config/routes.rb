3deea6d84ab7004020f6a23d3cb211f7911ddb21
Ryan Endacott <rzeg24@gmail.com>
2013-04-17 15:38:43 -0500

    got rid of testing routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,11 @@ Mizzouacm::Application.routes.draw do
 
 
   root :to => "home#index"
-  resources :users, :only => [:index, :show, :edit, :update, :hacks ]
+  resources :users, :only => [:index, :show, :edit, :update ]
   match '/auth/:provider/callback' => 'sessions#create'
   match '/signin' => 'sessions#new', :as => :signin
   match '/signout' => 'sessions#destroy', :as => :signout
   match '/auth/failure' => 'sessions#failure'
-  resources :projects
 
   get ':action' => 'static#:action'
 end
