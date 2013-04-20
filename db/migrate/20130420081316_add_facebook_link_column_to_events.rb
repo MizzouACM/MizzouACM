83104020ec333af0693a725966909233cc68366f
Ryan Endacott <rzeg24@gmail.com>
2013-04-20 03:24:11 -0500

    Added facebook link column to events

---
+++ db/migrate/20130420081316_add_facebook_link_column_to_events.rb
@@ -0,0 +1,5 @@
+class AddFacebookLinkColumnToEvents < ActiveRecord::Migration
+  def change
+    add_column :events, :facebook_link, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130420015916) do
+ActiveRecord::Schema.define(:version => 20130420081316) do
 
   create_table "events", :force => true do |t|
     t.string   "name"
     t.datetime "date"
     t.string   "location"
     t.text     "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+    t.string   "facebook_link"
   end
 
   create_table "users", :force => true do |t|
