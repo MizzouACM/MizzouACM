ec40b116c67649df28ca356d3ebd8ca638cf53ee
Ryan Endacott <rzeg24@gmail.com>
2013-04-20 03:33:47 -0500

    Front end for facebook event links

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  attr_accessible :date, :description, :location, :name
+  attr_accessible :date, :description, :location, :name, :facebook_link
 
   validates_presence_of :date, :description, :location, :name
 
