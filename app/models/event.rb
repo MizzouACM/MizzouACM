2a1f6da1f4e230257c281ccac2c7288d15ae1ccb
Ryan Endacott <rzeg24@gmail.com>
2013-04-21 14:44:30 -0500

    Made Redcarpet markdown object only get instantiated once

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,17 @@ class Event < ActiveRecord::Base
 
   validates_presence_of :date, :description, :location, :name
 
+  @@markdown = Redcarpet::Markdown.new(
+    Redcarpet::Render::HTML,
+    :autolink => true,
+    :space_after_headers => true)
+
+  # Markdown renderer for the event descriptions.
+  # May want to eventually make this into a presave hook for Event model
+  # to save it as description_rendered or something.  
+  # That way it isn't rendering on each page load.
+  def rendered_description
+    @@markdown.render(description)
+  end
+
 end
