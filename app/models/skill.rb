331dec0f4de6e2f65b61a62a46b451e045ad3546
Dan Silver <das2c3@mail.missouri.edu>
2013-06-02 14:54:47 -0500

    Skill icons

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -3,12 +3,17 @@ class Skill < ActiveRecord::Base
   attr_accessible :name
   validates :name, :uniqueness => {:scope => 'user_id', :message=> 'You cannot add the same skill twice.'}
   def self.valid_skills
-    ['Web Dev', 'C++', 'JavaScript']
+    {'Web Dev'=>"web.png", 'C++'=>"cplusplus.png", 'JavaScript'=>"js.png"}
   end
 
   def self.valid_skills_dropdown
-    ['Select a new skill to add'] + valid_skills
+    ['Select a new skill to add'] + valid_skills_keys
   end
-  validates_inclusion_of :name, :in => self.valid_skills
-
+  def self.valid_skills_keys
+  	self.valid_skills.keys
+  end
+  validates_inclusion_of :name, :in => self.valid_skills_keys
+  def self.getIcon(skill)
+  	return self.valid_skills[skill]
+  end
 end
