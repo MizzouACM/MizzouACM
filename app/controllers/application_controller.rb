7c5774ec7348a6dc9a4e0febf2b97bf2668a0880
Ryan Endacott <rzeg24@gmail.com>
2013-06-02 14:16:08 -0500

    Added verification that user adding skill is current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user
   helper_method :user_signed_in?
-  helper_method :correct_user?
   helper_method :user_must_be_admin
   helper_method :user_is_admin?
+  helper_method :user_is_current_user?
+  helper_method :user_must_be_current_user
 
   private
     def current_user
@@ -19,13 +20,6 @@ class ApplicationController < ActionController::Base
       return true if current_user
     end
 
-    def correct_user?
-      @user = User.find(params[:id])
-      unless current_user == @user
-        redirect_to root_url, :alert => "Access denied."
-      end
-    end
-
     def authenticate_user!
       if !current_user
         redirect_to root_url, :alert => 'You need to sign in for access to this page.'
@@ -34,6 +28,15 @@ class ApplicationController < ActionController::Base
 
     def user_must_be_admin
       redirect_to root_url, :alert => 'You must be a master hacker do that.' unless current_user and current_user.is_admin?
+    end
+
+    def user_is_current_user?
+      @user ||= User.find(params[:id] || params[:user_id])
+      return current_user == @user
+    end
+
+    def user_must_be_current_user
+      redirect_to user_path(params[:user_id]), :alert => 'Access denied.' unless user_is_current_user?
     end
 
     def user_is_admin?
--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,4 +1,7 @@
 class SkillsController < ApplicationController
+
+  before_filter :user_must_be_current_user
+
   def create
     @user = User.find(params[:user_id])
     @new_skill = @user.skills.new(params[:skill])
