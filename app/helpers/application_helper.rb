5c874b243169f0f14326a19720fa9ca26465c18f
Ryan Endacott <rzeg24@gmail.com>
2013-06-02 04:27:31 -0500

    removed render activity helper from applicationhelpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,21 +8,4 @@ module ApplicationHelper
     end
   end
 
-  def render_activity(activity)
-
-    content_tag(:p) do
-      case activity
-      when User
-        activity.name + " joined!"
-      when Event
-        activity.name + " announced!"
-      when Project
-        activity.title + " was created!"
-      else
-        raise "Activity type of " + activity.class.to_s + " not implemented in render_activity view helper"
-      end
-    end
-
-  end
-
 end
