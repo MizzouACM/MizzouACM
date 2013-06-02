2fffb741134c1c838cbfd37dbc753b53c75f3b00
Ryan Endacott <rzeg24@gmail.com>
2013-06-01 22:18:06 -0500

    Super simple not scalable backend for news feed implemented

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    events = Event.all
+    users = User.all
+    @feed = (events + users).sort_by(&:created_at)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,26 @@
 module ApplicationHelper
-  def nav_link(link_text, link_path)
-  class_name = current_page?(link_path) ? 'current' : ''
 
-  content_tag(:li, :class => class_name) do
-    link_to link_text, link_path
+  def nav_link(link_text, link_path)
+    class_name = current_page?(link_path) ? 'current' : ''
+
+    content_tag(:li, :class => class_name) do
+      link_to link_text, link_path
+    end
   end
-end
+
+  def render_activity(activity)
+
+    content_tag(:p) do
+      case activity
+      when User
+          activity.name + " joined!"
+      when Event
+        activity.name + " announced!"
+      else
+        "Error!"
+      end
+    end
+
+  end
+
 end
