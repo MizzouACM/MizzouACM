3cede2d6c3a46103d3da69ab00198d3f6d7103fc
Dan Silver <das2c3@mail.missouri.edu>
2013-06-23 16:53:07 -0500

    Front page content, markdown support for post summaries and bodies

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,15 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   attr_accessible :body, :title, :user_id, :summary
+  @@markdown = Redcarpet::Markdown.new(
+    Redcarpet::Render::HTML,
+    :autolink => true,
+    :space_after_headers => true)
+
+  def markdown_summary
+    @@markdown.render self.summary
+  end
+  def markdown_body
+    @@markdown.render self.body
+  end
 end
