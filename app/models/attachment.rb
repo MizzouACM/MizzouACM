285aa479d94671c8fa32ad90e2e60b456debbc79
Dan Silver <das2c3@mail.missouri.edu>
2013-06-30 00:45:25 -0500

    Storing the pictures on Amazon

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,6 +1,11 @@
 class Attachment < ActiveRecord::Base
   attr_accessible :image
   belongs_to :attachable, polymorphic: true
-  has_attached_file :image, :styles => { :large => "800x800>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image,
+                    :styles => { :large => "800x800>", :medium => "300x300>", :thumb => "100x100>" },
+                    :storage => :s3,
+                    :s3_credentials => { :access_key_id => ENV['access_key_id'], :secret_access_key => ENV['secret_access_key'], :bucket => "acm_pictures"},
+                    :path => ":attachment/:id/:style.:extension",
+                    :bucket => 'acm_pictures'
 
 end
