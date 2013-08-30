7d40a32b146ea9782693f7db7fc0810d09d76e72
Dan Silver <das2c3@mail.missouri.edu>
2013-08-30 16:17:38 -0500

    Updated menu links

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(:version => 20130829204745) do
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
     t.string   "facebook_link"
+    t.integer  "attachment_id"
   end
 
   create_table "groups", :force => true do |t|
